67fd62a1fc8866bb6255f0bf7ef2099702f1c466
Calamar_Robot <xanbrion1606@gmail.com>
2026-03-03 01:26:59 +0100

    Increase Pinch level to feat bigger circles Increase rasterization level to fit such zoom level Created keystore and implement app signature Implement dynamic thumbnail circles

--- LearningCircles/LearningCircleModel.cs
+++ LearningCircles/LearningCircleModel.cs
@@ -5,4 +5,16 @@ public class LearningCircleModel
     public string Title { get; init; } = string.Empty;
     public string Subtitle { get; init; } = string.Empty;
     public string SvgFile { get; init; } = string.Empty;
+
+    public string ThumbnailFile
+    {
+        get
+        {
+            var dotIndex = SvgFile.LastIndexOf('.');
+            if (dotIndex <= 0)
+                return $"{SvgFile}_thumb";
+
+            return $"{SvgFile[..dotIndex]}_thumb{SvgFile[dotIndex..]}";
+        }
+    }
 }
